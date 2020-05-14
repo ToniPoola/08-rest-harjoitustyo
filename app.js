2ac014753768d68f85a39166883542e6f33aad56
Toni Poola <Toni.Poola@seamk.fi>
2020-05-14 20:09:10 +0300

    commit

--- app.js
+++ app.js
@@ -25,20 +25,20 @@ app.use((req, res, next) => {
 }); // GET /api/materials
 
 //CREATE
-app.post("/machining-parameter-set", machining_controller.api_post_machining_parameter_set); //machining
+app.post("/api/machining-parameter-set", machining_controller.api_post_machining_parameter_set); //machining
 
 // READ
-app.get("/machining-parameter-sets", machining_controller.api_get_machining_parameter_sets);
+app.get("/api/machining-parameter-sets", machining_controller.api_get_machining_parameter_sets);
 // READ by id
-app.get("/machining-parameter-set/:id", machining_controller.api_get_machining_parameter_set)
+app.get("/api/machining-parameter-set/:id", machining_controller.api_get_machining_parameter_set)
 
 // UPDATE
 //app.patch korvaa vain tietyt kentät
 //app.put korvaa koko tiedon
-app.put("/machining-parameter-set/:id", machining_controller.api_put_machining_parameter_set);
+app.put("/api/machining-parameter-set/:id", machining_controller.api_put_machining_parameter_set);
 
 // DELETE
-app.delete("/machining-parameter-set/:id", machining_controller.api_delete_machining_parameter_set);
+app.delete("/api/machining-parameter-set/:id", machining_controller.api_delete_machining_parameter_set);
 
 //polku clusteriin
 const database_uri = "mongodb+srv://server:LckmK9T1HrTr1gbb@cluster0-2t6ml.mongodb.net/test?retryWrites=true&w=majority";
